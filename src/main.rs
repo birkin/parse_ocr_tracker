2d825bfc3152575b93aef53c51cdedf8e7d7c1c6
Birkin James Diana <birkin.diana@gmail.com>
2024-04-02 21:49:22 -0400

    uses basic walkdir to find json files.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use walkdir::WalkDir;
+use std::path::Path;
+
+fn find_json_files<P: AsRef<Path>>(path: P) {
+    for entry in WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path().is_file() && e.path().extension().unwrap_or_default() == "json")
+    {
+        println!("{}", entry.path().display());
+    }
 }
+
+fn main() {
+    // Replace "path/to/directory" with the actual path you want to search.
+    find_json_files("./");
+}
+
+
+// fn main() {
+//     println!("Hello, world!");
+// }
