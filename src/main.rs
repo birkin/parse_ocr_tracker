d7e84ec46231d2fdf9d6cbd321e27f8f07139537
Birkin James Diana <birkin.diana@gmail.com>
2024-04-04 06:36:00 -0400

    example clap working version.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use walkdir::WalkDir;
 use std::path::Path;
 
+use clap::{arg, Command};
+
+
 fn find_json_files<P: AsRef<Path>>(path: P) {
     for entry in WalkDir::new(path)
         .into_iter()
@@ -12,6 +15,25 @@ fn find_json_files<P: AsRef<Path>>(path: P) {
 }
 
 fn main() {
+    // get args
+    let matches = Command::new("MyApp")
+        .version("1.0")
+        .about("Does awesome things")
+        .arg(arg!(--two <VALUE>).required(true))
+        .arg(arg!(--one <VALUE>).required(true))
+        .get_matches();
+
+    println!(
+        "two: {:?}",
+        matches.get_one::<String>("two").expect("required")
+    );
+    println!(
+        "one: {:?}",
+        matches.get_one::<String>("one").expect("required")
+    );
+
+
+
     // Replace "path/to/directory" with the actual path you want to search.
     find_json_files("./");
 }
