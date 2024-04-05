807c89f09c4d9fbb35f26b75f079d5fd11c0b982
Birkin James Diana <birkin.diana@gmail.com>
2024-04-04 22:03:22 -0400

    converts &String reference into a &str.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,48 @@
-// use std::path::Path;
-// use walkdir::WalkDir;
+use std::path::Path;
+use walkdir::WalkDir;
 
 use clap::{arg, Command};
 
-// fn find_json_files<P: AsRef<Path>>(path: P) {
-//     for entry in WalkDir::new(path)
-//         .into_iter()
-//         .filter_map(|e| e.ok())
-//         .filter(|e| e.path().is_file() && e.path().extension().unwrap_or_default() == "json")
-//     {
-//         println!("{}", entry.path().display());
-//     }
-// }
+fn find_json_files<P: AsRef<Path>>(path: P) {
+    for entry in WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path().is_file() && e.path().extension().unwrap_or_default() == "json")
+    {
+        println!("{}", entry.path().display());
+    }
+}
 
 fn main() {
     // get args -----------------------------------------------------
     let matches = Command::new("MyApp")
         .version("1.0z")
         .about("Walks source_dir_path and lists all json files.")
-        .arg(arg!(-s --source_dir_path <VALUE>).required(true)) 
+        .arg(arg!(-s --source_dir_path <VALUE>).required(true))
         .arg(arg!(-o --output_dir_path <VALUE>).required(true))
         .get_matches();
+    let source_dir_temp_ref: &String = matches
+        .get_one::<String>("source_dir_path")
+        .expect("required");
+    // let source_dir: String = source_dir_temp_ref.to_string();
+    let source_dir: &str = source_dir_temp_ref.as_str();
+    // let zz: () = source_dir;
+    println!("source-arg: {:?}", source_dir);
+
+    // println!(
+    //     "source-arg: {:?}",
+    //     matches.get_one::<String>("source_dir_path").expect("required")
+    // );
 
     println!(
-        "source-arg: {:?}",
-        matches.get_one::<String>("source_dir_path").expect("required")
-    );
-    println!(
         "output-arg: {:?}",
-        matches.get_one::<String>("output_dir_path").expect("required")
+        matches
+            .get_one::<String>("output_dir_path")
+            .expect("required")
     );
 
     // Replace "path/to/directory" with the actual path you want to search.
-    // find_json_files("./");
+    find_json_files("./");
 }
 
 // fn main() {
