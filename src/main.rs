bd1c6fe923e9fa33c36449aa87e9dced397331fd
Birkin James Diana <birkin.diana@gmail.com>
2024-04-05 21:07:27 -0400

    completes arg grab.

--- src/main.rs
+++ src/main.rs
@@ -21,30 +21,20 @@ fn main() {
         .arg(arg!(-s --source_dir_path <VALUE>).required(true))
         .arg(arg!(-o --output_dir_path <VALUE>).required(true))
         .get_matches();
+    // get source_dir -----------------------------------------------
     let source_dir_temp_ref: &String = matches
         .get_one::<String>("source_dir_path")
-        .expect("required");
-    // let source_dir: String = source_dir_temp_ref.to_string();
-    let source_dir: &str = source_dir_temp_ref.as_str();
-    // let zz: () = source_dir;
+        .expect("Failed to get required 'source_dir_path' argument.");
+    let source_dir: &str = source_dir_temp_ref.as_str(); // or... let source_dir: String = source_dir_temp_ref.to_string();
     println!("source-arg: {:?}", source_dir);
-
-    // println!(
-    //     "source-arg: {:?}",
-    //     matches.get_one::<String>("source_dir_path").expect("required")
-    // );
-
-    println!(
-        "output-arg: {:?}",
-        matches
-            .get_one::<String>("output_dir_path")
-            .expect("required")
-    );
-
-    // Replace "path/to/directory" with the actual path you want to search.
+    // get output_dir -----------------------------------------------
+    let output_dir_temp_ref: &String = matches
+        .get_one::<String>("output_dir_path")
+        .expect("Failed to get required 'output_dir_path' argument.");
+    let output_dir: &str = output_dir_temp_ref.as_str();
+    println!("output-arg: {:?}", output_dir);
+    // call worker function -----------------------------------------
     find_json_files("./");
 }
 
-// fn main() {
-//     println!("Hello, world!");
-// }
+// let zz: () = the_var;
