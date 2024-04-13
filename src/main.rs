d39341036c61ccde07fe1892e3a49817ab499fe8
Birkin James Diana <birkin.diana@gmail.com>
2024-04-13 19:41:11 -0400

    starts module refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod helper;
 mod logger; // enables the log_debug!() and log_info!() macros
 
 use clap::{arg, Command};
@@ -18,53 +19,53 @@ use walkdir::WalkDir;
 */
 include!(concat!(env!("OUT_DIR"), "/git_commit.rs")); // OUT_DIR is set by cargo; is the target dir; and is only available during build process
 
-/*  -----------------------------------------------------------------
-    Finds all files in the given directory that end with "ocr_complete.json" or "ingest_complete.json".
-    -----------------------------------------------------------------
-*/
-fn find_json_files<P: AsRef<Path>>(
-    path: P,
-) -> (Vec<PathBuf>, Vec<PathBuf>, Vec<PathBuf>, Vec<PathBuf>) {
-    // -- setup data-vectors
-    let mut ocr_complete_paths = Vec::new();
-    let mut ingest_complete_paths = Vec::new();
-    let mut error_paths = Vec::new();
-    let mut other_paths = Vec::new();
-    // -- take a walk
-    for entry in WalkDir::new(path)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| e.path().is_file())
-    {
-        let path = entry.into_path();
-        if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) {
-            if file_name.ends_with("ocr_complete.json") {
-                ocr_complete_paths.push(path);
-            } else if file_name.ends_with("ingest_complete.json") {
-                ingest_complete_paths.push(path);
-            } else if file_name.contains("error") {
-                error_paths.push(path);
-            } else {
-                other_paths.push(path);
-            }
-        }
-    }
-    // -- sort the vectors
-    ocr_complete_paths.sort_by(|a, b| a.as_path().cmp(b.as_path()));
-    ingest_complete_paths.sort_by(|a, b| a.as_path().cmp(b.as_path()));
-    // output counts
-    log_info!("len-ocr_complete_paths: {}", ocr_complete_paths.len());
-    log_info!("len-ingest_complete_paths: {}", ingest_complete_paths.len());
-    log_info!("len-error_paths: {}", error_paths.len());
-    log_info!("len-other_paths: {}", other_paths.len());
-    // -- return
-    (
-        ocr_complete_paths,
-        ingest_complete_paths,
-        error_paths,
-        other_paths,
-    )
-}
+// /*  -----------------------------------------------------------------
+//     Finds all files in the given directory that end with "ocr_complete.json" or "ingest_complete.json".
+//     -----------------------------------------------------------------
+// */
+// fn find_json_files<P: AsRef<Path>>(
+//     path: P,
+// ) -> (Vec<PathBuf>, Vec<PathBuf>, Vec<PathBuf>, Vec<PathBuf>) {
+//     // -- setup data-vectors
+//     let mut ocr_complete_paths = Vec::new();
+//     let mut ingest_complete_paths = Vec::new();
+//     let mut error_paths = Vec::new();
+//     let mut other_paths = Vec::new();
+//     // -- take a walk
+//     for entry in WalkDir::new(path)
+//         .into_iter()
+//         .filter_map(|e| e.ok())
+//         .filter(|e| e.path().is_file())
+//     {
+//         let path = entry.into_path();
+//         if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) {
+//             if file_name.ends_with("ocr_complete.json") {
+//                 ocr_complete_paths.push(path);
+//             } else if file_name.ends_with("ingest_complete.json") {
+//                 ingest_complete_paths.push(path);
+//             } else if file_name.contains("error") {
+//                 error_paths.push(path);
+//             } else {
+//                 other_paths.push(path);
+//             }
+//         }
+//     }
+//     // -- sort the vectors
+//     ocr_complete_paths.sort_by(|a, b| a.as_path().cmp(b.as_path()));
+//     ingest_complete_paths.sort_by(|a, b| a.as_path().cmp(b.as_path()));
+//     // output counts
+//     log_info!("len-ocr_complete_paths: {}", ocr_complete_paths.len());
+//     log_info!("len-ingest_complete_paths: {}", ingest_complete_paths.len());
+//     log_info!("len-error_paths: {}", error_paths.len());
+//     log_info!("len-other_paths: {}", other_paths.len());
+//     // -- return
+//     (
+//         ocr_complete_paths,
+//         ingest_complete_paths,
+//         error_paths,
+//         other_paths,
+//     )
+// }
 
 /*  -----------------------------------------------------------------
     Represents the structure of the OCR JSON tracker files being parsed.
